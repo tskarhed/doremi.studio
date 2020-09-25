c735553f055881b4e4cf2738759d37f78d9daeab
Oskar Tengberg <52031625+MrTengil@users.noreply.github.com>
2020-09-25 23:05:30 +0200

    Cancel no longer adds a note

--- src/firebase/auth/ProfileForm.tsx
+++ src/firebase/auth/ProfileForm.tsx
@@ -3,35 +3,42 @@ import { Input, Button } from '../../native';
 import theme from '../../theme.module.scss';
 
 export interface FormDTO {
-    displayName: string;
+  displayName: string;
 }
 
 interface FormProps {
-    initialValues:FormDTO;
-    onSubmit: (form: FormDTO) => void;
+  initialValues: FormDTO;
+  onSubmit: (form: FormDTO) => void;
 }
 
 const formStyles = {
-    backgroundColor: theme.primary,
-    padding: "10%",
-    borderRadius: "3px",
-    boxShadow: "2px 2px 5px grey"
-}
+  backgroundColor: theme.primary,
+  padding: '10%',
+  borderRadius: '3px',
+  boxShadow: '2px 2px 5px grey',
+};
 
-export const ProfileForm: FC<FormProps> = ({initialValues, onSubmit}) => {
-    const [ displayName, setDisplayName ] = useState(initialValues.displayName);
+export const ProfileForm: FC<FormProps> = ({ initialValues, onSubmit }) => {
+  const [displayName, setDisplayName] = useState(initialValues.displayName);
 
-return (
-<form style={formStyles} onSubmit={(event) => {
-    event.preventDefault();
-    onSubmit({displayName});
-}}>
-    <label htmlFor="displayName">Set your username</label>
-    <Input
+  return (
+    <form
+      style={formStyles}
+      onSubmit={(event) => {
+        event.preventDefault();
+        onSubmit({ displayName });
+      }}
+    >
+      <label htmlFor="displayName">Set your username</label>
+      <Input
         placeholder="Username"
         name="displayName"
         value={displayName}
-        onChange={(event: ChangeEvent<HTMLInputElement>) => setDisplayName(event.currentTarget.value)}/>
-        <Button>Submit</Button>
-</form>);
-}
+        onChange={(event: ChangeEvent<HTMLInputElement>) =>
+          setDisplayName(event.currentTarget.value)
+        }
+      />
+      <Button>Submit</Button>
+    </form>
+  );
+};
